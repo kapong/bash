890aca45600c8e35f33dce04bfc59c27e34fb7ce
Phongphan Phienphanich <garpong@gmail.com>
2025-08-29 08:12:38 +0700

    Update Ubuntu sources to use Thailand mirror

--- update_th_mirror.sh
+++ update_th_mirror.sh
@@ -2,3 +2,5 @@
 
 sed -i 's|http://archive.ubuntu.com/ubuntu|http://th.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list.d/ubuntu.sources
 sed -i 's|http://security.ubuntu.com/ubuntu|http://th.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list.d/ubuntu.sources
+
+apt update
